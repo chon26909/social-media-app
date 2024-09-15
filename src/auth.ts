563d354eb38b31fe3f1fc509527c8963fa2436ed
HiChon <59905927+chon26909@users.noreply.github.com>
2024-09-15 23:55:05 +0700

    validate session

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@lucia-auth/adapter-prisma"
 import prisma from "./lib/prisma"
 import { Lucia, Session, User } from 'lucia'
 import { cache } from "react"
+import { cookies } from "next/headers"
 
 const adapter = new PrismaAdapter(prisma.session, prisma.user)
 
@@ -42,9 +43,40 @@ export const validateRequest = cache(
     async (): Promise<
     { user: User; session: Session } | { user:null; session: null }
     > =>  {
-        return {
-            user: null,
-            session: null
+
+        const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+
+        if (!sessionId) {
+            return {
+                user: null,
+                session: null
+            }
         }
+
+        const result = await lucia.validateSession(sessionId)
+
+        try {
+            if (result.session && result.session.fresh) {
+                 const sessionCookie = lucia.createSessionCookie(result.session?.id)
+                 cookies().set(
+                    sessionCookie.name, 
+                    sessionCookie.value, 
+                    sessionCookie.attributes
+                 )
+            }
+
+            if (!result.session) {
+                const sessionCookie = lucia.createBlankSessionCookie()
+                cookies().set(
+                    sessionCookie.name, 
+                    sessionCookie.value, 
+                    sessionCookie.attributes
+                )
+            }
+        } catch (error) {
+            console.log("error: " + error);
+        }   
+
+        return result
     }
 )
